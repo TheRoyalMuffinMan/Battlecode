c71c60f27086c465ac78623f15676521db6ca055
Andy <85419676+TheRoyalMuffinMan@users.noreply.github.com>
2022-01-06 21:24:02 -0600

    Improvements

--- src/LittleMuffinBot/Miner.java
+++ src/LittleMuffinBot/Miner.java
@@ -1,21 +1,34 @@
 package LittleMuffinBot;
 
 import battlecode.common.*;
+import java.util.HashMap;
 import java.util.Map;
-import static LittleMuffinBot.RobotPlayer.deposits;
 
 public strictfp class Miner {
     static MapLocation location;
+    static int vision;
     static Direction exploreDir = null;
+    static Map<MapLocation, Integer> deposits = new HashMap<>();
 
     // Every turn, we will first scan with the miner then attempt to mine.
     // If we can't mine, we search then explore.
     static void dispatcher(RobotController rc) throws GameActionException {
         location = rc.getLocation();
+        vision = rc.getType().visionRadiusSquared;
         scanMode(rc);
         if (!mineMode(rc)) {
             searchMode(rc);
             exploreMode(rc);
+        }
+    }
+    // Scan for nearby deposits in the static map we collected from all the robots this turn.
+    static void scanMode(RobotController rc) throws GameActionException {
+        rc.setIndicatorString("Scanning");
+        MapLocation[] nearby = rc.senseNearbyLocationsWithLead(vision); // Bytecode: 100
+        for (MapLocation position : nearby) {
+            if (rc.senseLead(position) > 1) {
+                deposits.put(position, rc.senseLead(position));
+            }
         }
     }
 
@@ -62,7 +75,7 @@ public strictfp class Miner {
         }
     }
 
-    // Exploring is done here randoming, we attempt to move in 2 different completely random directions.
+    // Exploring is done here randomizing, we attempt to move in 2 different completely random directions.
     // We catch when we get stuck against the edges of the map.
     static void exploreMode(RobotController rc) throws GameActionException {
         if (exploreDir == null) {
@@ -79,50 +92,6 @@ public strictfp class Miner {
         Direction dir = RobotPlayer.directions[directionIndex];
         if (rc.canMove(dir)) {
             rc.move(dir);
-            System.out.println("I moved!");
-        }
-    }
-    // Scan for nearby deposits in the static map we collected from all the robots this turn.
-    static void scanMode(RobotController rc) throws GameActionException {
-        rc.setIndicatorString("Scanning");
-        int vision = rc.getType().visionRadiusSquared;
-        MapLocation[] nearby = rc.senseNearbyLocationsWithLead(vision); // Bytecode: 100
-        for (MapLocation position : nearby) {
-            if (rc.senseLead(position) > 10) {
-                deposits.put(position, rc.senseLead(position));
-            }
         }
     }
 }
-
-// Extra Code
-// Scan for nearby deposits
-//        int vision = rc.getType().visionRadiusSquared;
-//        MapLocation[] nearby = rc.getAllLocationsWithinRadiusSquared(me, vision);
-//
-//        // Find the shortest deposit at the given robot
-//        MapLocation deposit = null;
-//        int distanceToDeposit = Integer.MAX_VALUE;
-//        for (MapLocation position : nearby) {
-//            if (rc.senseLead(position) > 0 || rc.senseGold(position) > 0) {
-//                int distance = me.distanceSquaredTo(position);
-//                if (distance < distanceToDeposit) {
-//                    distanceToDeposit = distance;
-//                    deposit = position;
-//                    break;
-//                }
-//            }
-//        }
-//        // If the miner is mining, we shouldn\t move him
-//        if (!minerIsMining) {
-//            // Might need to remove deposit once we are done mining it!!!!! Note: static variables don't persist
-//            MapLocation deposit = searchForResources(deposits);
-//            if (deposit != null) {
-////              System.out.println("Miner moving!!");
-//                Direction toMove = me.directionTo(deposit);
-//                if (rc.canMove(toMove)) {
-//                    rc.move(toMove);
-//                }
-//            }
-//            explore(rc);
-//        }
